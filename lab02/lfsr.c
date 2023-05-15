0402c8ef9281cd753be50c41bb0282c7d7e40826
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-05-15 14:32:45 +0800

    feat: implement lfsr_calculate

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,9 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    // the Exclusive Or of the bits originally in position 0,2,3 and 5.
+    uint16_t bit = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1u;
+    // shift 1 bit the the right and set the leftmost bit to the Exclusive Or
+    *reg = (*reg >> 1) | (bit << 15);
 }
 
