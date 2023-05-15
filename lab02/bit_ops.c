47238b21491b6e63d2e7ad45243dfe01e69b4420
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-05-15 13:28:02 +0800

    refactor: set_bits

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -9,15 +9,7 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return (x >> n) & 01;
-}
-
-// Set the nth bits to v, others to 1
-unsigned mask_nth(unsigned n,
-                  unsigned v) {
-    //11100 ^ 00111 = 11011; ~(~0 << (n + 1))
-    //11000 ^ 00111 = 11111
-    return ((~v << n) ^ ~(~0 << (n + 1)));
+    return (x >> n) & 1;
 }
 
 // Set the nth bit of the value of x to v.
@@ -26,7 +18,8 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
-    *x = ((*x) | (1 << n)) & mask_nth(n, v); 
+    // *x = (*x | (v << n)) & ~((v ^ 1) << n);
+    *x = (*x | (1 << n)) & ~((v ^ 1) << n);
 }
 
 // Flip the nth bit of the value of x.
