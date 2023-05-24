9d669a25ce754123bae4ab24e6196691c036643e
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-05-24 15:25:11 +0800

    feat: implement evaluateOneCell determines color of cell by rule

--- gameoflife.c
+++ gameoflife.c
@@ -17,12 +17,44 @@
 #include <inttypes.h>
 #include "imageloader.h"
 
+
+int _state(Color **p, int bias) {
+    return (*(p + bias))->B & 1;
+}
+
+int _adjacentState(Image *image, int raw_row, int raw_col) {
+    Color **p = image->image;
+    int cols = image->cols;
+    int rows = image->rows;
+    int row = (raw_row < 0) ? rows - 1 : ((raw_row > rows - 1) ? 0 : raw_row);
+    int col = (raw_col < 0) ? cols - 1 : ((raw_col > cols - 1) ? 0 : raw_col);
+    int state = _state(p, row * cols + col);
+    return state; 
+}
+
 //Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
 //Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
 Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 {
 	//YOUR CODE HERE
+    //Assume the rule is a hexadecimal number between 0x00000 and 0x3FFFF.
+    Color **p = image->image;
+    int mine = row * image->cols + col;
+    //State: alive(1) or dead(0)
+    int my_state = _state(p, mine); 
+    //The number of alive neighbors
+    int alive_n = _adjacentState(image, row - 1, col) + _adjacentState(image, row + 1, col)
+                  + _adjacentState(image, row, col - 1) + _adjacentState(image, row, col + 1)
+                  + _adjacentState(image, row - 1, col - 1) + _adjacentState(image, row + 1, col + 1) 
+                  + _adjacentState(image, row - 1, col + 1) + _adjacentState(image, row + 1, col - 1);
+    //Determines color by rule
+    Color *decoded;
+    if ((decoded = (Color *) malloc(sizeof(Color))) == NULL) {
+        exit(-1);
+    }
+    decoded->R = decoded->G = decoded->B = 255 * (rule >> (9 * my_state + alive_n) & 1);
+    return decoded;
 }
 
 //The main body of Life; given an image and a rule, computes one iteration of the Game of Life.
@@ -30,6 +62,8 @@ Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
 Image *life(Image *image, uint32_t rule)
 {
 	//YOUR CODE HERE
+    //Assume the rule is a hexadecimal number between 0x00000 and 0x3FFFF.
+    return image;
 }
 
 /*
@@ -50,4 +84,17 @@ You may find it useful to copy the code from steganography.c, to start.
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+    Image *image = readData(argv[1]);
+    writeData(image);
+    printf("\nThe next generation of the file is: \n");
+    for (int i = 0; i < image->rows; i += 1) {
+        for (int j = 0; j < image->cols - 1; j += 1) {
+            Color *decoded = evaluateOneCell(image, i, j, 0x1808);
+            printf("%3hhu %3hhu %3hhu   ", decoded->R, decoded->G, decoded->B);
+            free(decoded);
+        }
+        Color *decoded = evaluateOneCell(image, i, image->cols - 1, 0x1808);
+        printf("%3hhu %3hhu %3hhu\n", decoded->R, decoded->G, decoded->B);
+        free(decoded);
+    }
 }
